7d1d25fd0b350fbeb698d1a610cc36b59a2803f9
unknown <jahanyu@gmail.com>
2024-08-08 16:48:18 -0500

    first Commit

---
+++ src/app http.ts
@@ -0,0 +1,30 @@
+import http from 'http'
+import fs from 'fs'
+
+const server = http.createServer(( req, res ) => {
+
+    console.log(req.url);
+
+    //res.write('Hola Mundo');
+    //res.end();
+
+    //const data = { name: 'Jahan Barraza', Age: 39, city: 'Barranquilla'}
+    //res.writeHead(200, { 'Content-Type': 'application/json' });
+    //res.end( JSON.stringify(data))
+
+    if ( req.url === '/') {
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8')
+        res.writeHead(200, {'Content-Type': 'text/html'} );
+        res.end( htmlFile )
+    }else {
+        res.writeHead(404, { 'Content-Type':'text/html'});
+        res.end()
+    }
+
+})
+
+
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080');
+})
